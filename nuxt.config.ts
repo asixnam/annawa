1a246c0577938292ad966d5c32d7571566539636
Asix1234 <asixartmedia@gmail.com>
2026-02-05 14:53:20 +0700

    icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,8 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap' }
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap' },
+        { rel: 'icon', type: 'image/png', href: '/ico.png' }
       ]
     }
   },
