97029cd2004b83189509f72b7b48215201c607ef
Asix1234 <asixartmedia@gmail.com>
2026-03-04 05:37:18 +0700

    asd

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'annawa',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  ssl: process.env.DB_HOST && process.env.DB_HOST.includes('aivencloud') ? { rejectUnauthorized: true } : undefined,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
